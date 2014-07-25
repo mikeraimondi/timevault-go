0869503e468bbcb766bfec8e4ce354064db431ee
Michael Raimondi <zeno@pobox.com>
2014-07-24 22:27:26 -0400

    Initial implementation of User struct

--- time-vault/time-vault.go
+++ time-vault/time-vault.go
@@ -2,12 +2,20 @@ package timevault
 
 import (
         "net/http"
+        "time"
 
         "appengine"
+        "appengine/datastore"
         "appengine/user"
 
         "fmt"
 )
+
+type TimevaultUser struct {
+  ID string
+  PhoneNumber string
+  Created time.Time
+}
 
 func init() {
         http.HandleFunc("/", root)
@@ -16,7 +24,31 @@ func init() {
 func root(w http.ResponseWriter, r *http.Request) {
         c := appengine.NewContext(r)
         if u := user.Current(c); u != nil {
-          fmt.Fprintf(w, `Hello, %s`, u)
+          q := datastore.NewQuery("TimevaultUser").Filter("ID =", u.ID).Limit(1)
+          users := make([]TimevaultUser, 0, 1)
+          if _, err := q.GetAll(c, &users); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+          }
+          curUser := &TimevaultUser{}
+          if len(users) == 0 {
+            newUser := &TimevaultUser{
+              ID: u.ID,
+              PhoneNumber: "555-5555",
+              Created: time.Now(),
+            }
+            key := datastore.NewIncompleteKey(c, "TimevaultUser", nil)
+            if _, err := datastore.Put(c, key, newUser); err != nil {
+              http.Error(w, err.Error(), http.StatusInternalServerError)
+              return
+            }
+            curUser = newUser
+          } else {
+            curUser = &users[0]
+          }
+
+          fmt.Fprintf(w, `Hello, %s `, u)
+          fmt.Fprintf(w, `Phone #: %s`, curUser.PhoneNumber)
           return
         } else {
           url, _ := user.LoginURL(c, "/")
