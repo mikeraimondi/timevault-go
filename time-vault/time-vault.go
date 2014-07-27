e29f2f816371783501d98be2fcf5e21195f28602
Michael Raimondi <zeno@pobox.com>
2014-07-26 21:02:29 -0400

    Complete wrapping of User API struct with custom struct

--- time-vault/time-vault.go
+++ time-vault/time-vault.go
@@ -1,58 +1,67 @@
 package timevault
 
 import (
-        "net/http"
-        "time"
+	"net/http"
+	"time"
 
-        "appengine"
-        "appengine/datastore"
-        "appengine/user"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
 
-        "fmt"
+	"fmt"
 )
 
 type TimevaultUser struct {
-  ID string
-  PhoneNumber string
-  Created time.Time
+	ID          string
+	Email       string
+	Username    string
+	PhoneNumber string
+	Created     time.Time
+}
+
+func (u *TimevaultUser) String() string {
+	return u.Username
 }
 
 func init() {
-        http.HandleFunc("/", root)
+	http.HandleFunc("/", root)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-        c := appengine.NewContext(r)
-        if u := user.Current(c); u != nil {
-          q := datastore.NewQuery("TimevaultUser").Filter("ID =", u.ID).Limit(1)
-          users := make([]TimevaultUser, 0, 1)
-          if _, err := q.GetAll(c, &users); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-          }
-          curUser := &TimevaultUser{}
-          if len(users) == 0 {
-            newUser := &TimevaultUser{
-              ID: u.ID,
-              PhoneNumber: "555-5555",
-              Created: time.Now(),
-            }
-            key := datastore.NewIncompleteKey(c, "TimevaultUser", nil)
-            if _, err := datastore.Put(c, key, newUser); err != nil {
-              http.Error(w, err.Error(), http.StatusInternalServerError)
-              return
-            }
-            curUser = newUser
-          } else {
-            curUser = &users[0]
-          }
+	c := appengine.NewContext(r)
+	if u := user.Current(c); u != nil {
+		q := datastore.NewQuery("TimevaultUser").Filter("ID =", u.ID).Limit(1)
+		users := make([]TimevaultUser, 0, 1)
+		if _, err := q.GetAll(c, &users); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		curUser := &TimevaultUser{}
+		if len(users) == 0 {
+			newUser := &TimevaultUser{
+				ID:          u.ID,
+				Email:       u.Email,
+				Username:    u.String(),
+				PhoneNumber: "555-5555",
+				Created:     time.Now(),
+			}
+			key := datastore.NewIncompleteKey(c, "TimevaultUser", nil)
+			if _, err := datastore.Put(c, key, newUser); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			curUser = newUser
+		} else {
+			curUser = &users[0]
+		}
 
-          fmt.Fprintf(w, `Hello, %s `, u)
-          fmt.Fprintf(w, `Phone #: %s`, curUser.PhoneNumber)
-          return
-        } else {
-          url, _ := user.LoginURL(c, "/")
-          fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
-          return
-        }
+		fmt.Fprintf(w, `Hello, %s `, curUser)
+		fmt.Fprintf(w, `Phone #: %s`, curUser.PhoneNumber)
+		fmt.Fprintf(w, `Email #: %s`, curUser.Email)
+		return
+	} else {
+		url, _ := user.LoginURL(c, "/")
+		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+		return
+	}
 }
