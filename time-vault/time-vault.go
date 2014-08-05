c3b45cc6fe9e657f72f902838f50d63e2b0eec12
Michael Raimondi <zeno@pobox.com>
2014-08-04 22:26:10 -0400

    Redirect on auth

--- time-vault/time-vault.go
+++ time-vault/time-vault.go
@@ -70,7 +70,7 @@ func authenticate(w http.ResponseWriter, r *http.Request) (*TimevaultUser, error
 		}
 	} else {
 		url, _ := user.LoginURL(c, "/")
-		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+		http.Redirect(w, r, url, 302)
 		return &TimevaultUser{}, nil
 	}
 }
