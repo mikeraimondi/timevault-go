fce4fafb6457e8196c8cb0d9a6130f00f58202b3
Michael Raimondi <mike@mikeraimondi.com>
2014-08-29 21:56:40 -0400

    Go fmt

--- time-vault/Pomodoro.go
+++ time-vault/Pomodoro.go
@@ -3,9 +3,9 @@ package timevault
 import "time"
 
 type Pomodoro struct {
-  User       string
-  Duration   int64
-  CreatedAt  time.Time
-  Finished   bool
-  FinishedAt time.Time
+	User       string
+	Duration   int64
+	CreatedAt  time.Time
+	Finished   bool
+	FinishedAt time.Time
 }
--- time-vault/TimeVaultUser.go
+++ time-vault/TimeVaultUser.go
@@ -3,13 +3,13 @@ package timevault
 import "time"
 
 type TimevaultUser struct {
-  ID          string
-  Email       string
-  Username    string
-  PhoneNumber string
-  CreatedAt   time.Time
+	ID          string
+	Email       string
+	Username    string
+	PhoneNumber string
+	CreatedAt   time.Time
 }
 
 func (u *TimevaultUser) String() string {
-  return u.Username
+	return u.Username
 }
