c755a94fbd6d90009b9456b7d8c8ee93d3823564
Michael Raimondi <mike@mikeraimondi.com>
2014-09-06 15:14:37 -0400

    Refactor pomodoro notification task to use Delay package

--- time-vault/time-vault.go
+++ time-vault/time-vault.go
@@ -8,6 +8,7 @@ import (
 
 	"appengine"
 	"appengine/datastore"
+	"appengine/delay"
 	"appengine/taskqueue"
 	"appengine/user"
 )
@@ -15,7 +16,6 @@ import (
 func init() {
 	http.HandleFunc("/users", withAuth(users))
 	http.HandleFunc("/pomodoros", withAuth(index))
-	http.HandleFunc("/endpomodoro", endPomodoro)
 }
 
 func withAuth(handler func(http.ResponseWriter, *http.Request, *TimevaultUser)) http.HandlerFunc {
@@ -88,60 +88,49 @@ func index(w http.ResponseWriter, r *http.Request, u *TimevaultUser) {
 		Finished:  false,
 	}
 	key := datastore.NewIncompleteKey(c, "Pomodoro", u.CachedKey)
-	if key, err := datastore.Put(c, key, newPomodoro); err != nil {
+	key, err = datastore.Put(c, key, newPomodoro)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	} else {
-		// TODO use Delay package?
-		t := taskqueue.NewPOSTTask("/endpomodoro", map[string][]string{"key": {key.Encode()}})
-		t.Delay = newPomodoro.Duration
-		if _, err := taskqueue.Add(c, t, ""); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(newPomodoro)
 	}
+	t, err := endPomodoro.Task(*key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Delay = newPomodoro.Duration
+	if _, err := taskqueue.Add(c, t, ""); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(newPomodoro)
 }
 
-// TODO admin-only auth
-func endPomodoro(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		c := appengine.NewContext(r)
-		config, err := getConfig(&c)
-		if err != nil {
-			c.Errorf("%v", err)
-			return
-		}
-		var pom Pomodoro
-		if key, err := datastore.DecodeKey(r.FormValue("key")); err != nil {
-			c.Errorf("%v", err)
-			return
-		} else {
-			if err := datastore.Get(c, key, &pom); err != nil {
-				c.Errorf("%v", err)
-				return
-			}
-			if pom.Finished {
-				c.Warningf("%v", "Pomodoro already completed")
-				return
-			}
-			pom.Finished = true
-			pom.FinishedAt = time.Now()
-			if _, err := datastore.Put(c, key, &pom); err != nil {
-				c.Errorf("%v", err)
-				return
-			}
-			var u TimevaultUser
-			if err := datastore.Get(c, key.Parent(), &u); err != nil {
-				c.Errorf("%v", err)
-				return
-			}
-			if _, err := sendTwilioMessage(config.TwilioNumber, u.PhoneNumber, "Pomodoro complete!", &c); err != nil {
-				c.Errorf("%v", err)
-				return
-			}
-			return
-		}
+var endPomodoro = delay.Func("endPomodoro", func(c appengine.Context, k datastore.Key) error {
+	config, err := getConfig(&c)
+	if err != nil {
+		return err
 	}
-}
+	var pom Pomodoro
+	if err := datastore.Get(c, &k, &pom); err != nil {
+		return err
+	}
+	if pom.Finished {
+		c.Warningf("%v", "Pomodoro already completed")
+		return nil
+	}
+	pom.Finished = true
+	pom.FinishedAt = time.Now()
+	if _, err := datastore.Put(c, &k, &pom); err != nil {
+		return err
+	}
+	var u TimevaultUser
+	if err := datastore.Get(c, k.Parent(), &u); err != nil {
+		return err
+	}
+	if _, err := sendTwilioMessage(config.TwilioNumber, u.PhoneNumber, "Pomodoro complete!", &c); err != nil {
+		return err
+	}
+	return nil
+})
